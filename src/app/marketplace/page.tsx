c665258535c2823697d818662db3e53032ae1ec7
PARJANYA RAM <parjanya14@gmail.com>
2025-10-14 14:58:12 +0000

    perefect now reduce the price a lil so that we the target audience can a

--- src/app/marketplace/page.tsx
+++ src/app/marketplace/page.tsx
@@ -5,14 +5,14 @@ import { PlaceHolderImages } from '@/lib/placeholder-images';
 import { useLanguage } from '@/context/language-context';
 
 const mockProducts = [
-  { id: '1', name: 'product_denim_tote', price: '3,800', artisan: 'artisan_rural_weavers', image: PlaceHolderImages.find(img => img.id === 'product-1') },
-  { id: '2', name: 'product_patchwork_rug', price: '7,200', artisan: 'artisan_creative_threads', image: PlaceHolderImages.find(img => img.id === 'product-2') },
-  { id: '3', name: 'product_silk_scarf', price: '2,500', artisan: 'artisan_artisan_alliance', image: PlaceHolderImages.find(img => img.id === 'product-3') },
-  { id: '4', name: 'product_remnant_wallet', price: '1,800', artisan: 'artisan_eco_crafters', image: PlaceHolderImages.find(img => img.id === 'product-4') },
-  { id: '5', name: 'product_woven_placemats', price: '2,100', artisan: 'artisan_rural_weavers', image: PlaceHolderImages.find(img => img.id === 'product-5') },
-  { id: '6', name: 'product_teddy_bear', price: '1,500', artisan: 'artisan_creative_threads', image: PlaceHolderImages.find(img => img.id === 'product-6') },
-  { id: '7', name: 'product_fabric_bouquet', price: '2,900', artisan: 'artisan_artisan_alliance', image: PlaceHolderImages.find(img => img.id === 'product-7') },
-  { id: '8', name: 'product_fabric_coasters', price: '1,200', artisan: 'artisan_eco_crafters', image: PlaceHolderImages.find(img => img.id === 'product-8') },
+  { id: '1', name: 'product_denim_tote', price: '2,500', artisan: 'artisan_rural_weavers', image: PlaceHolderImages.find(img => img.id === 'product-1') },
+  { id: '2', name: 'product_patchwork_rug', price: '4,800', artisan: 'artisan_creative_threads', image: PlaceHolderImages.find(img => img.id === 'product-2') },
+  { id: '3', name: 'product_silk_scarf', price: '1,600', artisan: 'artisan_artisan_alliance', image: PlaceHolderImages.find(img => img.id === 'product-3') },
+  { id: '4', name: 'product_remnant_wallet', price: '1,200', artisan: 'artisan_eco_crafters', image: PlaceHolderImages.find(img => img.id === 'product-4') },
+  { id: '5', name: 'product_woven_placemats', price: '1,500', artisan: 'artisan_rural_weavers', image: PlaceHolderImages.find(img => img.id === 'product-5') },
+  { id: '6', name: 'product_teddy_bear', price: '950', artisan: 'artisan_creative_threads', image: PlaceHolderImages.find(img => img.id === 'product-6') },
+  { id: '7', name: 'product_fabric_bouquet', price: '1,800', artisan: 'artisan_artisan_alliance', image: PlaceHolderImages.find(img => img.id === 'product-7') },
+  { id: '8', name: 'product_fabric_coasters', price: '750', artisan: 'artisan_eco_crafters', image: PlaceHolderImages.find(img => img.id === 'product-8') },
 ];
 
 export default function MarketplacePage() {
